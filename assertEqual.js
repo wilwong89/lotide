b593d8d0d3dc00b02f980ba24cb025061940c819
Wilson Wong <wilwong89@gmail.com>
2020-01-06 22:36:59 +0000

    Added assertEqual.js file

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+assertEqual("Bootcamp", "Bootcamp");
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
