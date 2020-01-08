19b11971d40165965829d0f764a072fa5e25e42b
Wilson Wong <wilwong89@gmail.com>
2020-01-08 18:19:23 +0000

    Created findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} + " !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(tvShowObject = {}, inputValue = "",) {
+
+  for (const key in tvShowObject) {
+    if (tvShowObject[key] === inputValue) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
